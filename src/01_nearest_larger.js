f0c8c3cd37063f1500dc50075aca783238f15ca7
alonzoalayon <alonzo@campgladiator.com>
2016-05-08 14:44:44 -0500

    expect not defined

--- src/01_nearest_larger.js
+++ src/01_nearest_larger.js
@@ -1,4 +1,19 @@
+
 function nearestLarger(arr, i) {
+  var i;
+  var arr = [];
+  var x = i + 1;
+  //console.log('ehllo');
+   for (var j = i - 1; j >= 0; i--) {
+       if (arr[i] < arr[j]) {
+           return j;
+           console.log(j);
+       } else if (arr[i] < arr[x]) {
+           return x;
+           console.log(x);
+       }
+       x++;
+   }
 
 }
 
