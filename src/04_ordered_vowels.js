21c216b13fb0240d3ef1fc93ca3e0c88411aee19
alonzoalayon <alonzo@campgladiator.com>
2016-05-30 09:57:09 -0500

    passing

--- src/04_ordered_vowels.js
+++ src/04_ordered_vowels.js
@@ -1,5 +1,36 @@
-function orderedVowelWords(str) {
-
+function orderedVowel(str) {
+    //split word
+    var wordArray = str.split(" ");
+    //hold vowels
+    var correctVowels = [];
+    for (var i = 0; i < wordArray.length; i++) {
+        if (isItOrdered(wordArray[i])) {
+            correctVowels.push(wordArray[i]);
+        }
+    }
+    return correctVowels.join(" ");
 }
 
-module.exports = orderedVowelWords;
+//helper function
+function isItOrdered(string) {
+    //array of ordered vowels
+    var re = /[aeiou]/i;
+    var vowels = [];
+    var sorted = [];
+    //check if string matches orderered vowels
+    for (var i = 0; i < string.length; i++) {
+        if (string[i].match(re)) {
+            vowels.push(string[i]);
+            sorted.push(string[i]);
+        }
+    }
+
+    sorted.sort();
+    if (sorted.toString() === vowels.toString()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = orderedVowel;
